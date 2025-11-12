a9326032c3e91dc86b906c928b1bf2701b19b414
Constance Neeser <TheRiver@users.noreply.github.com>
2025-11-12 09:17:52 +0200

    feat: first version takes text and returns an url

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,7 @@
-export function bob(name: string) : string {
-    return "Hello " + name;
+import {v5} from 'uuid';
+
+const MENTION_NAMESPACE_UUID = '019a768a-995f-768d-9bb2-eee17e46dc24';
+
+export function mention_v5_from_text(name: string) : string {
+    return "https://uri.dataeq.com/v1/" + v5(name, MENTION_NAMESPACE_UUID);
 }
